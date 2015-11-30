9c41ab4fd4188f3bc9dae225abb749c7654b7d0b
domicb <domi1213@hotmail.com>
2015-11-30 14:39:01 +0100

    Mostramos tarea a completar

--- app/views/completar.php
+++ app/views/completar.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Shop Item - Start Bootstrap Template</title>
-
+    <?php require_once('\\..\\models\\funciones.php');?>
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
     <!-- Versión compilada y comprimida del JavaScript de Bootstrap -->
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
@@ -53,9 +53,7 @@
     </nav>
 
     <!-- Page Content -->
-    <div class="container">
-
-        
+    <div class="container">    
 
             <div class="col-md-3"><br>
                 <p class="lead">Menú</p>
@@ -67,15 +65,12 @@
                     <a href="../controllers/completar_tarea.php" class="list-group-item">Finalizar Tarea</a>
                     <a href="../controllers/borrar.php" class="list-group-item">Borrar Tarea</a>
                 </div>
-            </div><br>
-            <!--         cabecera        -->
-                
-                        <p><h2>Completar Tarea</h2></p>                           
-                    <hr> 
-                  
+            </div><br><br><br>
+            <!--         cabecera        -->          
+                <p><h2>Completar Tarea</h2></p>                           
+                    <hr>          
                  <div class="row">
-                    <div class="col-md-8">
-                        
+                    <div class="col-md-8">                        
                     <!-- FORMULARIO -->
                     <form name="nuevo_usuario" class="form" method="POST" action="nueva_tarea.php">              
                             <!-- radio estado -->
@@ -91,18 +86,55 @@
                                   <input type="radio" name="estado" value="cancelada"> Cancelada
                                 </label> <?php if( isset($array_errores['estado']) )
                                     { VerError('estado'); }?>   </p>
-                            </div>
-                                 
+                            </div>                                
                                 <div class="form-group">
                                     <input type="text" class="form-control"  name="anotaciones_anteriores" value="<?=ValorPost('descripcion')?>" placeholder="Anotaciones" />
                                 </div>
-
-			<hr> <center><input type="submit" name="completar" value="Completar" /></center>
-																
-			</form>
-                        
+			<hr> <center><input type="submit" name="completar" value="Completar" /></center>																
+			</form>                       
                     </div>
-                 </div>   
+                 </div> <!-- Se cierra div formulario -->
+                    <div class="row">
+                        <div class="col-md-3 .col-md-offset-3">
+                     <table class="table table-striped">
+            			<thead>
+            				<tr><!--tabla de tareas-->
+            					<td><b>Descripcion</b></td>
+            					<td><b>Operario</b></td>
+                                                <td><b>Nombre</b></td>
+                                                <td><b>Codigo Postal</b></td>
+                                                <td><b>Telefono</b></td>
+                                                <td><b>Provincia</b></td>
+                                                <td><b>Direccion</b></td>
+                                                <td><b>Poblacion</b></td>
+                                                <td><b>Estado</b></td>
+                                                <td><b>Fecha Creacion</b></td>
+                                                <td><b>Fecha Finalizacion</b></td>
+                			</tr>
+                				<tbody>
+                                    <tr>                                                                     
+                                    <tr>
+                                    <td> <?php echo $tarea['descripcion'];?> </td>
+                                    <td> <?php echo $tarea['operario'];?> </td>
+                                    <td> <?php echo $tarea['nombre'];?> </td>
+                                    <td> <?php echo $tarea['cod_postal'];?> </td>
+                                    <td> <?php echo $tarea['telefono'];?> </td>
+                                    <!-- sacamos el id de la provincia el cual nos sirve para preguntar por su nombre  -->                         
+                                    <td> <?php echo getProvincia($tarea['idprovincia']);?> </td>             
+                                            
+                                    <td> <?php echo $tarea['direccion'];?> </td>
+                                    <td> <?php echo $tarea['poblacion'];?> </td>
+                                    <td> <?php echo getEstado($tarea['estado']);?> </td>
+                                    <td> <?php echo $tarea['fecha_creacion'];?> </td>
+                                    <td> <?php echo $tarea['fecha_realizacion'];?> </td>                             
+                                </tr>
+                        
+            				</tbody>
+            		</table>                         
+                                                         
+                        </div>
+
+                    </div>
                 
 
             </div>
